8acc93822ae2e95e1335857d815141bb7ddbd06a
Maria2009gift <kovalenkom2009@gmail.com>
2024-09-03 18:37:26 +0300

    add style

--- src/Recipes/Recipes.jsx
+++ src/Recipes/Recipes.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
-import { RecipeInfo } from "../RecipeInfo/Recipe";
+import { RecipeInfo } from "../RecipeInfo/RecipeInfo";
+import styles from "./Recipes.module.css";
 
 export const Recipes = ({ recipes }) => {
     return (
-        <ul>
+        <ul className={styles.list}>
             {recipes.map(recipe =>
-                <li key={recipe.id}>
+                <li key={recipe.id} className={styles.item}>
                     <RecipeInfo recipe={recipe} />
                 </li>)}
         </ul>
